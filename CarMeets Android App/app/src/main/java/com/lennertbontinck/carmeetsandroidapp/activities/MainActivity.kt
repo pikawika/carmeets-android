25fa3ac232ae4370e981c03a8dbd9ca20264bbb2
Lennert Bontinck <lennertbontinck@live.be>
2018-11-03 23:32:31 +0100

    topbar met custom notificatiebel etc
    topbar heeft backknop, notificatiebel dat een kleintekstveld heeft voor aantal weer te geven en poc om aantal aan te passen alsook een zoekknop

--- CarMeets Android App/app/src/main/java/com/lennertbontinck/carmeetsandroidapp/activities/MainActivity.kt
+++ CarMeets Android App/app/src/main/java/com/lennertbontinck/carmeetsandroidapp/activities/MainActivity.kt
@@ -3,6 +3,11 @@ package com.lennertbontinck.carmeetsandroidapp.activities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import com.lennertbontinck.carmeetsandroidapp.R
 import com.lennertbontinck.carmeetsandroidapp.fragments.AccountFragment
 import com.lennertbontinck.carmeetsandroidapp.fragments.FavorietenlijstFragment
@@ -24,35 +29,73 @@ class MainActivity : AppCompatActivity() {
         initListeners()
     }
 
+    //bel en zoek toevoegen aan app bar
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+
+        //manueel listener event voor notificatiebel met badge adhv actionLayout
+        val notificaties = menu?.findItem(R.id.nav_notificaties)?.actionView
+        notificaties?.findViewById<ImageView>(R.id.nav_notificatiebel)?.setOnClickListener({ notificatiesClicked() })
+        notificaties?.findViewById<TextView>(R.id.nav_notificatiebel_aantal)?.setOnClickListener({ notificatiesClicked() })
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            //bel icoon ga naar liked meetings
+            R.id.nav_notificaties -> {
+                notificatiesClicked()
+                return super.onOptionsItemSelected(item)
+            }
+
+            R.id.nav_zoek -> {
+                Toast.makeText(this, "Er is op zoeken geklikt", Toast.LENGTH_SHORT).show()
+                return super.onOptionsItemSelected(item)
+            }
+
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
     private fun initApp() {
         setSupportActionBar(toolbar)
+
 
         //initieel is meetinglijst geselecteerd
         bottom_navigation.selectedItemId = R.id.nav_meetings
 
         //initieel meetinglijst fragment tonen
+        supportActionBar?.title = "Meetings"
+        supportActionBar?.subtitle = "Alle meetings"
         supportFragmentManager.beginTransaction()
             .add(R.id.fragment_container, MeetinglijstFragment())
             .commit()
 
-        //initieele titel van toolbar instellen
-        supportActionBar?.title = "Meetings"
-        supportActionBar?.subtitle = "Alle meetings"
     }
 
     private fun initListeners() {
-        toolbar.setNavigationOnClickListener {
-            supportFragmentManager.popBackStack()
-            supportActionBar?.title = getString(R.string.app_name)
-            supportActionBar?.setDisplayHomeAsUpEnabled(false)
-        }
-
         bottom_navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+    }
+
+    private fun notificatiesClicked() {
+        //POC dat je aantal kan aanpassen, +1 op click
+        val notificatieAantal = toolbar.menu.findItem(R.id.nav_notificaties)?.actionView?.findViewById<TextView>(R.id.nav_notificatiebel_aantal)
+
+        notificatieAantal?.text = (notificatieAantal?.text.toString().toInt() + 1).toString()
+
+
+        supportActionBar?.title = "Favorieten"
+        supportActionBar?.subtitle = "Liked en going meetings"
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, FavorietenlijstFragment())
+            .commit()
+        bottom_navigation.selectedItemId = R.id.nav_favorieten
     }
 
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        supportFragmentManager.popBackStack()
-        when (item.itemId) {
+        when (item?.itemId) {
             //meetinglijst geselecteerd
             R.id.nav_meetings -> {
                 supportActionBar?.title = "Meetings"
@@ -65,7 +108,7 @@ class MainActivity : AppCompatActivity() {
             //favorieten geselecteerd
             R.id.nav_favorieten -> {
                 supportActionBar?.title = "Favorieten"
-                supportActionBar?.subtitle = "Meetings waarin je geïnteresseerd bent"
+                supportActionBar?.subtitle = "Liked en going meetings"
                 supportFragmentManager.beginTransaction()
                     .replace(R.id.fragment_container, FavorietenlijstFragment())
                     .commit()
@@ -74,7 +117,7 @@ class MainActivity : AppCompatActivity() {
             //account geselecteerd
             R.id.nav_account -> {
                 supportActionBar?.title = "Account"
-                supportActionBar?.subtitle = "Beheer je instellingen"
+                supportActionBar?.subtitle = "Instellingen aanpassen"
                 supportFragmentManager.beginTransaction()
                     .replace(R.id.fragment_container, AccountFragment())
                     .commit()
